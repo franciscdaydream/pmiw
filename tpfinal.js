3aa92dc1da176a4327b9b02a65a6a0b77d2a3798
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2024-10-15 11:47:01 -0300

    tpfinal.js

--- tpfinal.js
+++ tpfinal.js
@@ -10,7 +10,7 @@ function preload() {
   logo = loadImage("data/logo.png");
   ///////////////////////////////////////////IMAGENES
 
- for (let i=0; i<10; i++) {
+  for (let i=0; i<10; i++) {
     iamgenes[i] = loadImage("data/imagen"+i+".png");
   }
 }
@@ -59,7 +59,7 @@ function mouseClicked() {
     estado = "nar5a";
   } else if (mouseX > 60 && mouseX < 60+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar5a") {
     estado = "nar6a";
-    } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar4a") {
+  } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar4a") {
     estado = "op2";
   } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar3") {
     estado = "op2";
@@ -71,7 +71,6 @@ function mouseClicked() {
     estado = "fb1";
   } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="juego") {
     estado = "fm1";
-    
   } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar5a") {
     estado = "fm2";
   } else if (mouseX > 340 && mouseX < 340+250 && mouseY > 420 && mouseY < 420+50 && estado=="nar6a") {
@@ -92,7 +91,7 @@ function mouseClicked() {
     estado = "creditos";
   } else if (mouseX > 160 && mouseX < 160+300 && mouseY > 300 && mouseY < 300+80 && estado=="fm2") {
     estado = "creditos";
-    } else if (mouseX > 160 && mouseX < 160+300 && mouseY > 300 && mouseY < 300+80 && estado=="creditos") {
+  } else if (mouseX > 160 && mouseX < 160+300 && mouseY > 300 && mouseY < 300+80 && estado=="creditos") {
     estado = "menu";
   }
 }
@@ -112,52 +111,60 @@ function setup() {
 function draw() {
   background(0);
   print(estado);
-  stroke(37,58,88);
+  stroke(37, 58, 88);
   strokeWeight(2);
-  
-  
+
+
   if ( estado == "menu") {
     fill(255);
     textSize(55);
-    image(logo,130,0,350,150);
+    image(logo, 130, 0, 350, 150);
     textSize(40);
     text("El capitulo olvidado", 160, 150);
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(160, 300, 300, 80);
     fill(255);
     text("Comenzar", 220, 355);
   } else if ( estado == "nar0") {
 
-    
-    fill(352,78,40);
+
+    fill(352, 78, 40);
     rect(70, height/2, 510, 170);
     rect(340, 420, 250, 50);
     textSize(20);
     fill(255);
     dialogo(75, height/2+10, 'na1');
+    dialogo(75, height/2+40, 'na1a');
+    dialogo(75, height/2+70, 'na1b');
+    dialogo(75, height/2+100, 'na1c');
     text("continuar...", 370, 455);
   } else if ( estado == "nar1") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
     fill(255);
     textSize(20);
     dialogo(75, height/2+10, 'op1');
+    dialogo(75, height/2+40, 'op1a');
+    dialogo(75, height/2+70, 'op1b');
     text("continuar...", 370, 455);
     text("arrepentirse", 80, 455);
   } else if ( estado == "nar2") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
     fill(255);
     textSize(20);
     dialogo(75, height/2+10, 'na2');
+    dialogo(75, height/2+40, 'na2a');
+    dialogo(75, height/2+70, 'na2b');
+    dialogo(75, height/2+100, 'na2c');
     text("Adentrarse", 370, 455);
     text("Pelear", 80, 455);
   } else if ( estado == "nar3") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -167,7 +174,7 @@ function draw() {
     text("Derecha", 370, 455);
     text("Izquierda", 80, 455);
   } else if ( estado == "nar4a") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -177,7 +184,7 @@ function draw() {
     text("Derecha", 370, 455);
     text("Izquierda", 80, 455);
   } else if ( estado == "nar5a") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -187,7 +194,7 @@ function draw() {
     text("Derecha", 370, 455);
     text("Izquierda", 80, 455);
   } else if ( estado == "nar6a") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -197,7 +204,7 @@ function draw() {
     text("Derecha", 370, 455);
     text("Izquierda", 80, 455);
   } else if ( estado == "fb2") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     textSize(55);
     text("Final bueno 2", 120, 100);
     rect(160, 300, 300, 80);
@@ -205,7 +212,7 @@ function draw() {
     textSize(40);
     text("Creditos", 240, 355);
   } else if ( estado == "fm2") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     textSize(55);
     text("Final malo 2", 120, 100);
     rect(160, 300, 300, 80);
@@ -213,7 +220,7 @@ function draw() {
     textSize(40);
     text("Creditos", 240, 355);
   } else if ( estado == "op2") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -223,7 +230,7 @@ function draw() {
     text("Avanzar en soledad", 370, 455);
     text("Ser acomapañado", 80, 455);
   } else if ( estado == "nar4b") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     rect(70, height/2, 510, 170);
@@ -236,7 +243,7 @@ function draw() {
     fill(255);
     textSize(55);
     text("Final bueno 3", 120, 100);
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(160, 300, 300, 80);
     fill(255);
     textSize(40);
@@ -245,7 +252,7 @@ function draw() {
     fill(255);
     textSize(55);
     text("Final bueno 1", 120, 100);
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(160, 300, 300, 80);
     fill(255);
     textSize(40);
@@ -254,7 +261,7 @@ function draw() {
     fill(255);
     textSize(55);
     text("Final malo 1", 120, 100);
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(160, 300, 300, 80);
     fill(255);
     textSize(40);
@@ -270,7 +277,7 @@ function draw() {
     text("Iara Vilte - 94714/4", 80, 230);
     textSize(20);
     text("Creacion  Warren Ellis - Basado en  Castlevania de Konami", 55, 270);
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(160, 300, 300, 80);
     fill(255);
     textSize(40);
@@ -280,14 +287,14 @@ function draw() {
   ///////////////////////PENDIENTE
 
   if ( estado == "juego") {
-    fill(352,78,40);
+    fill(352, 78, 40);
     rect(340, 420, 250, 50);
     rect(60, 420, 250, 50);
     fill(255);
     textSize(20);
     noStroke();
     text("esta parte esta incompleta, al menos por ahora", 50, 50)
-    stroke(37,58,88);
+      stroke(37, 58, 88);
     text("perder", 370, 455);
     text("ganar", 80, 455);
   }
