8f9825b9863162d86e9df8a6c0dc7ff6ee977fc5
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2024-11-27 21:35:25 -0300

    Update tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,7 +1,7 @@
 /* TP final etapa 2 
   Alumno: Franco D'Angelo 
   Legajo: 88224/2
-  Link de youtube:
+  Link de youtube: https://youtu.be/yOUnG-LqiDs
 */
 let juegoFunc, sonam, sonlat, tnr
 let imagen = [];
